643b1a6d98c933a12d6e3c23e5d1d17b1fb050ac
sowasp <sowsmartool@gmail.com>
2026-06-21 19:43:56 +0200

    create js variables with the respective querySelector to select each id and class needed

---
+++ script.js
@@ -0,0 +1,8 @@
+const display = document.querySelector("#display")
+const numberButtons = document.querySelectorAll(".number")
+const operator = document.querySelectorAll(".operator")
+const clear = document.querySelector("#clear")
+const del = document.querySelector("#delete")
+const point = document.querySelector("#point")
+const isEqual = document.querySelector("#equal")
+
